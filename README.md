3554ba927576bac6109497b8107455fc98fbf2ae
Hallez <jbhallez@aol.com>
2016-11-14 11:57:34 +0100

    Ephéméride
    Annonce "la Saint" du Jour

---
+++ README.md
@@ -0,0 +1,24 @@
+<!---
+IMPORTANT
+=========
+This README.md is displayed in the WebStore as well as within Jarvis app
+Please do not change the structure of this file
+Fill-in Description, Usage & Author sections
+Make sure to rename the [en] folder into the language code your plugin is written in (ex: fr, es, de, it...)
+For multi-language plugin:
+- clone the language directory and translate commands/functions.sh
+- optionally write the Description / Usage sections in several languages
+-->
+## Description
+Annonce un proverbe insrit dans le fichier config.sh
+
+## Usage
+```
+You: éphéméride ?
+Jarvis: On est le 15 novembre, c'est la Saint Albert
+
+```
+
+## Author
+[Jean-Bernard Hallez](https://github.com/Jean-Bernard-Hallez/jarvis-plugin-math.git)
+
---
+++ fr/functions.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+saint () {
+local lejour=$(LC_TIME="$language.UTF-8" date +"%d")
+local lemois=$(LC_TIME="$language.UTF-8" date +"%m") 
+
+if [[ "$lemois" == "01" ]]; then
+local saint_config="$saint_config_jan"
+local moisentier="Janvier"
+fi
+
+if [[ "$lemois" == "02" ]]; then
+local saint_config="$saint_config_fev"
+local moisentier="Février"
+fi
+
+if [[ "$lemois" == "03" ]]; then
+local saint_config="$saint_config_mar"
+local moisentier="Mars"
+fi
+
+if [[ "$lemois" == "04" ]]; then
+local saint_config="$saint_config_avr"
+local moisentier="Avril"
+
+fi
+
+if [[ "$lemois" == "05" ]]; then
+local saint_config="$saint_config_mai"
+local moisentier="Mai"
+
+fi
+
+if [[ "$lemois" == "06" ]]; then
+local saint_config="$saint_config_jui"
+local moisentier="Juin"
+
+fi
+
+if [[ "$lemois" == "07" ]]; then
+local saint_config="$saint_config_juillet"
+local moisentier="Juillet"
+
+fi
+
+if [[ "$lemois" == "08" ]]; then
+local saint_config="$saint_config_aou"
+local moisentier="Aout"
+
+fi
+
+if [[ "$lemois" == "09" ]]; then
+local saint_config="$saint_config_sep"
+local moisentier="Septembre"
+
+fi
+
+if [[ "$lemois" == "10" ]]; then
+local saint_config="$saint_config_oct"
+local moisentier="Octobre"
+fi
+
+if [[ "$lemois" == "11" ]]; then
+local saint_config="$saint_config_nov"
+local moisentier="Novembre"
+
+fi
+
+if [[ "$lemois" == "12" ]]; then
+local saint_config="$saint_config_dec"
+local moisentier="Decembre"
+
+fi
+
+saint_conf
+}
+
+
+
+saint_conf () {
+echo "on est le : $lejour $moisentier"
+while read device
+do
+local jour="$(jv_sanitize "$device" ".*")"
+if [[ "$jour" == "$lejour" ]]; then
+local nom="$(echo "$saint_config" | jq -r ".devices[] | select(.jour==\"$device\") | .nom")"
+echo "C'est la Saint $nom"
+fi
+done <<< "$(echo "$saint_config" | jq -r '.devices[].jour')"
+}
+
+
+
+
